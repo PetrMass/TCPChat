8793518183c091b0436c503fa6cfe980730a0753
Петр <massyutenko@mail.ru>
2020-05-08 09:15:26 +0300

    Объедениел проекты. Создал gitignore

---
+++ TCPClient/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace TCPСlient
+{
+    class Program
+    {
+        private const string host = "127.0.0.1";
+        private const int port = 8888;
+        static TcpClient client;
+        static NetworkStream stream;
+
+        static void Main(string[] args)
+        {
+            client = new TcpClient();
+            try
+            {
+                client.Connect(host, port); //подключение клиента
+                stream = client.GetStream(); // получаем поток
+                
+                Thread receiveThread = new Thread(ReceiveMessage); // запускаем новый поток для получения данных
+                receiveThread.Start();
+                Console.WriteLine("Соединение с сервером установлено");
+                SendMessage();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Disconnect();
+            }
+        }
+        
+        static void SendMessage() // отправка сообщений
+        {         
+            while (true)
+            {
+                string message = Console.ReadLine();
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+            }
+        }
+        
+        static void ReceiveMessage() // получение сообщений
+        {
+            while (true)
+            {
+                try
+                {
+                    byte[] data = new byte[64]; 
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+
+                    string message = builder.ToString();
+                    Console.WriteLine(message);
+                }
+                catch
+                {
+                    Console.WriteLine("Подключение прервано!"); //соединение было прервано
+                    Console.ReadLine();
+                    Disconnect();
+                }
+            }
+        }
+
+        static void Disconnect()
+        {
+            if (stream != null)
+                stream.Close();//отключение потока
+            if (client != null)
+                client.Close();//отключение клиента
+            Console.WriteLine("дисконект");
+            Console.ReadKey();
+        }
+    }
+}
